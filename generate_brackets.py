aa16f311d69e0e656347f9e7bdaede50fa4703cb
alex.proskurnin <alexey.proskurnin@gmail.com>
2025-06-01 22:26:55 +0300

    formatting

--- generate_brackets.py
+++ generate_brackets.py
@@ -2,6 +2,7 @@ def generate_brackets():
     n = int(input())
     generate_all_seq("", 0, 0, n)
 
+
 def generate_all_seq(curr_seq, curr_left, curr_right, n):
     if len(curr_seq) == 2 * n:
         print(curr_seq)
